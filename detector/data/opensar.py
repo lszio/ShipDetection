35f8dec4e26735a71872157ed7aeb5acea4bc35a
Liszt21 <1832666492@qq.com>
2020-05-21 09:37:04 +0800

    fix datadict opensar

--- detector/data/opensar.py
+++ detector/data/opensar.py
@@ -2,7 +2,6 @@ import os
 from pathlib import Path
 import math
 from PIL import Image
-# import cv2
 import xml.etree.ElementTree as ET
 from detectron2.data import DatasetCatalog, MetadataCatalog
 from detectron2.structures import BoxMode
@@ -43,36 +42,15 @@ def label_transform(label_in, level=1):
 
 
 def get_opensar_dicts(path, img_type='vv', level=0, part="all", rotated=True):
-    imgs = {'vv': [], 'vh': []}
     targets = []
-    num_images = 0
     folders = [folder for folder in path.glob("S*")]
-    sorted(folders)
     assert part in ['all', 'train', 'test']
+    num_anns = 0
+    num_chips = 0
+    num_imgs = 0
+    temp = []
     for folder in folders:
-        img_dict = {}
-        img_size_dict = {}
-        if not (folder / "chip_size.txt").exists():
-            for folder in path.iterdir():
-                with open(folder / "chip_size.txt", 'w') as f:
-                    for img in (folder / "Patch_Uint8").iterdir():
-                        x, y = img.name.split("_")[-3:-1]
-                        name = x + "_" + y
-                        size = Image.open(img).size
-                        f.write("{} {}\n".format(name, size[0]))
-        for folder in path.iterdir():
-            with open(folder / "chip_size.txt", 'r') as f:
-                for line in f.readlines():
-                    item = line.strip().split(" ")
-                    img_size_dict[item[0]] = item[1]
-        for image in (folder / "Patch_Uint8").iterdir():
-            if image.name[0] == '.':
-                continue
-            x, y, t = image.name.split("_")[-3:]
-            img_dict["{}_{}_{}".format(x[1:], y[1:],
-                                       t[:2])] = (image,
-                                                  img_size_dict[x + "_" + y])
-            num_images += 1
+        targets = {}
         root = ET.parse(folder / "ship.xml").getroot()
         for ship in root:
             target = {}
@@ -84,38 +62,57 @@ def get_opensar_dicts(path, img_type='vv', level=0, part="all", rotated=True):
             trainfo = ship[3]
             target['length'] = float(trainfo[0].text)
             target['width'] = float(trainfo[1].text)
-            targets.append(target)
-            imgs['vv'].append(img_dict["{}_{}_vv".format(*target['center'])])
-            imgs['vh'].append(img_dict["{}_{}_vh".format(*target['center'])])
+            targets['{}_{}'.format(*target['center'])] = target
+            num_anns += 1
+
+        img_size_dict = {}
+        if not (folder / "chip_size.txt").exists():
+            with open(folder / "chip_size.txt", 'w') as f:
+                for img in (folder / "Patch_Uint8").glob('*'):
+                    x, y = img.name.split("_")[-3:-1]
+                    name = x + "_" + y
+                    size = Image.open(img).size
+                    f.write("{} {}\n".format(name, size[0]))
+        with open(folder / "chip_size.txt", 'r') as f:
+            for line in f.readlines():
+                item = line.strip().split(" ")
+                img_size_dict[item[0]] = item[1]
+        num_chips += len(img_size_dict)
+        for image in (folder / "Patch_Uint8").glob('*'):
+            x, y, t = image.name.split("_")[-3:]
+            num_imgs += 1
+            target = targets["{}_{}".format(x[1:], y[1:])]
+            target['file_name'] = str(image)
+            size = int(img_size_dict["{}_{}".format(x, y)])
+            target['width'] = size
+            target['height'] = size
+            temp.append(target)
 
     dataset_dicts = []
-    for index, img_info in enumerate(imgs[img_type]):
+    for index, target in enumerate(temp):
         record = {}
-        img, size = img_info
-        filename = str(img)
-
-        record["file_name"] = filename
+        record["file_name"] = target['file_name']
         record["image_id"] = index
-        height = width = int(size)
+        height, width = target['height'], target['width']
         record["height"] = height
         record["width"] = width
         cx, cy = width / 2, height / 2
-        ht = targets[index]['ht']
-        box = targets[index]['box']
+        ht = target['ht']
+        box = target['box']
         w = ((ht[2] - ht[0])**2 + (ht[3] - ht[1])**2)**0.5
         ang = math.asin(abs(ht[3] - ht[1]) / w)
         a = ang / 3.1415926 * 180
         if (ht[2] - ht[0]) * (ht[3] - ht[1]) > 0:
             a = -a
         w = w / (box[2] - box[0]) * width
-        h = w / targets[index]['length'] * targets[index]['width']
+        h = w / target['length'] * target['width']
         if rotated:
             obj = {
                 "bbox": [cx, cy, w, h, a],
                 "bbox_mode":
                 BoxMode.XYWHA_ABS,
                 "category_id":
-                label_transform(targets[index]['label'], level=level),
+                label_transform(target['label'], level=level),
                 "iscrowd":
                 0
             }
@@ -128,7 +125,7 @@ def get_opensar_dicts(path, img_type='vv', level=0, part="all", rotated=True):
                 "bbox_mode":
                 BoxMode.XYXY_ABS,
                 "category_id":
-                label_transform(targets[index]['label'], level=level),
+                label_transform(target['label'], level=level),
                 "iscrowd":
                 0
             }
@@ -146,10 +143,13 @@ def get_opensar_dicts(path, img_type='vv', level=0, part="all", rotated=True):
     dataset_dicts = []
     for category_id, category in categories.items():
         num_ship = len(category)
-        print(num_ship)
         dataset_dicts += category[:-num_ship //
                                   5] if part != 'test' else category[
                                       -num_ship // 5:]
+    # print('num_anns={}'.format(num_anns))
+    # print('num_chips={}'.format(num_chips))
+    # print('num_imgs={}'.format(num_imgs))
+    # print('len(temp)={}'.format(len(temp)))
     return dataset_dicts
 
 
@@ -182,4 +182,7 @@ if __name__ == "__main__":
     register_opensar_datasets()
     DatasetCatalog.list()
     dataset = DatasetCatalog.get("opensar_1_train")
+    print(len(dataset))
+    dataset = DatasetCatalog.get("opensar_1_test")
+    print(len(dataset))
     print(dataset[0])
